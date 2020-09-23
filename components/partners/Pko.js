a079bc5201ffe7ce89f65b2d56eaabe5af94164e
kacapka <wojciech.urbansk@gmail.com>
2020-09-23 12:10:38 +0200

    last fixes

--- components/partners/Pko.js
+++ components/partners/Pko.js
@@ -5,17 +5,19 @@ import pkoLogo from '../../static/img/pko_logo.jpg';
 
 const Content = () => (
     <p className="bird-text margin-top">
-        PKO Bank Polski is the undisputed leader of the Polish banking sector. The bank is the primary provider of financial services for all customer segments, achieving the highest shares in the savings and loans market, leasing market, and private equity funds. With over 9.4 million payment cards, the bank is the largest issuer of debit and credit cards in Poland. Thanks to the development of digital tools, including the IKO application, which at the end of the first quarter of 2020 had 4.5 million active applications, PKO Bank Polski is the most mobile bank in Poland. The companies of the PKO Bank Polski Group strengthen the Bank's strong position. The Brokerage House of PKO Banku Polskiego is a leader in terms of the number and value of IPO and SPO transactions on the capital market.
-    </p>
+        Having over 9.46 million pay cards, the Bank is the largest issuer of debit and credit cards in Poland. Thanks to the development of digital tools, including IKO, which was activated almost 4.7 million times by the end of the first half of 2020, PKO Bank Polski has become the most mobile bank in Poland. The Capital Group companies reinforce the Bank’s strong position. PKO Bank Polski Brokerage House is the leader with respect to the number and the value of IPO and SPO transactions on the capital market.    </p>
 );
 
 const Pko = () => {
-    const { isMobile } = useMedia();
+    const { isTablet } = useMedia();
 
     return (
         <div className="container--small pko">
             <img src={pkoLogo} className="pko-logo" alt="PKO Bank Polski" />
-            {isMobile ? <CollapseContent><Content /></CollapseContent> : <Content />}
+            <p className="bird-text">
+                PKO Bank Polski is the indisputable leader in the Polish banking sector. The Bank is the basic supplier of financial services for all segments of clients, having highest shares in the market of deposits, credit, lease and investment funds for natural persons.
+            </p>
+            {isTablet ? <CollapseContent><Content /></CollapseContent> : <Content />}
         </div>
     );
 };
--- components/policy/Content.js
+++ components/policy/Content.js
@@ -2,77 +2,18 @@ const Content = () => {
 
     return (
         <div className="container--small content">
-            <p className="content-text with-margin">
-                Fundacja na rzecz innowacji finansowych FinTech Polska (FinTech Poland Foundation) based in Warsaw Poland and registered under KRS (registry) number 588844 is the controller of your personal data.
+            <p className="content-text with-margin center">
+                <span className="bold">Privacy Policy of Fundacja na rzecz innowacji finansowych FinTech Polska</span> <br />
+                based in Warsaw Poland and registered under KRS (registry) number 588844
             </p>
             <p className="content-text with-margin">
-                This policy sets out the basis on which any personal data we collect from you will be processed by us. Please read the following carefully to understand our views and practices regarding your personal data and how we will treat it. By visiting our website and using our services you consent to the practices described in this policy.
-            </p>
-            <p className="content-title">
-                Personal data we process
-            </p>
-            <p className="content-text with-margin">
-                In order to acquire “How to do FinTech in Poland” report and receive the marketing information, you will be asked for basic contact details such as email address and your organisation name.
+                Please read the following carefully to understand our views and practices regarding your personal data and how we will treat it. By visiting our website and using our services you consent to the practices described in this policy.
             </p>
             <p className="content-title">
                 Cookies
             </p>
             <p className="content-text with-margin">
-                Our website may use cookies to distinguish you from other users of our website. This helps us to provide you with a good experience when you browse our website and also allows us to improve our site. For detailed information on the cookies we use and the purposes for which we use them see our Cookie Policy below.
-            </p>
-
-            <p className="content-title">
-                How we use your personal data
-            </p>
-            <p className="content-text with-margin">
-                We use your personal data to provide you with “How to do FinTech in Poland” report and marketing information regarding our and our business partners’ activity.
-            </p>
-
-            <p className="content-title">
-                Basis of your personal data processing
-            </p>
-            <p className="content-text with-margin">
-                We process your personal data on basis of your consent. You can withdraw the consent at any time.
-            </p>
-
-            <p className="content-title">
-                Disclosure of your personal data
-            </p>
-            <p className="content-text with-margin">
-                We may share your personal data with our subcontractors, including analytics and search engine providers that assist us in the improvement and optimisation of our website.
-            </p>
-
-            <p className="content-title">
-                Place of your personal data processing
-            </p>
-            <p className="content-text with-margin">
-                Your personal data are processed shall be processed destination in Poland or in other county within the European Economic Area ("EEA").
-            </p>
-
-            <p className="content-title">
-                Your rights
-            </p>
-            <p className="content-text with-margin">
-                You have the following specific rights in respect of the personal data we process: <br />
-                1. The right to be informed about how we use your personal data - This Privacy Policy explains who we are; the purposes for which we process personal data and our legitimate interests in so doing; the categories of data we process; third party disclosures; <br />
-                2. The right of access to your personal data we hold; <br />
-                3. The right to rectification where data are inaccurate or incomplete; <br />
-                4. The right to erasure of your personal data, but only in very specific circumstances, typically where your personal data are no longer necessary in relation to the purpose for which it was originally collected or processed; or, in certain cases where we have relied on consent to process the data, when that consent is withdrawn and there is no other legitimate reason for continuing to process that data; or when the individual objects to the processing and there is no overriding legitimate interest for continuing the processing; <br />
-                5. The right to restrict processing, for example while we are reviewing the accuracy or completeness of data, or deciding on whether any request for erasure is valid. In such cases we shall continue to store the data, but not further process it until such time as we have resolved the issue; <br />
-                6. The right to object in cases where processing is based on legitimate interests, where our requirement to process the data is overridden by the rights of the individual concerned; or for the purposes of direct marketing (including profiling); or for processing for purposes of scientific / historical research and statistics, unless this is for necessary for the performance of a public interest task. <br />
-            </p>
-            <p className="content-text with-margin">
-                You can also exercise the right at any time by contacting us at <a href="mailto:info@fintechpoland.com">info@fintechpoland.com</a>.
-            </p>
-            <p className="content-text with-margin">
-                Our website or documents presented at our website may, from time to time, contain links to and from the websites of our partner networks, advertisers and affiliates. If you follow a link to any of these websites, please note that these websites have their own privacy policies and that we do not accept any responsibility or liability for these policies. Please check these policies before you submit any personal data to these websites.
-            </p>
-            <p className="content-text with-margin">
-                If you have any demands, inquiries or complaints about data protection at FinTech Poland Foundation please contact us at <a href="mailto:info@fintechpoland.com">info@fintechpoland.com</a>.
-            </p>
-
-            <p className="content-title">
-                About cookies
+                Our website may use cookies to distinguish you from other users of our website. This helps us to provide you with a good experience when you browse our website and also allows us to improve our site.
             </p>
             <p className="content-text with-margin">
                 Cookies are pieces of information that a website transfers to your computer’s hard disk for record-keeping purposes. Cookies can make the internet more useful by storing information about your preferences on a particular site, such as your personal preference pages.
@@ -80,12 +21,94 @@ const Content = () => {
             <p className="content-text with-margin">
                 The use of cookies is an industry standard, and most websites use them to provide useful features for their customers. Cookies in and of themselves do not personally identify users, although they do identify a user’s computer. Most browsers are initially set to accept cookies.
             </p>
-            <p className="content-text with-margin">
-                If you would prefer, you can set yours to refuse cookies. However, you may not be able to take full advantage of a website if you do so.
+            <p className="content-text">
+                If you would prefer, you can set yours to refuse cookies. However, you may not be able to take full advantage of a website if you do so
             </p>
-
         </div>
     );
 };
 
 export default Content;
+
+
+{/* <p className="content-text with-margin">
+Fundacja na rzecz innowacji finansowych FinTech Polska (FinTech Poland Foundation) based in Warsaw Poland and registered under KRS (registry) number 588844 is the controller of your personal data.
+</p>
+<p className="content-text with-margin">
+This policy sets out the basis on which any personal data we collect from you will be processed by us. Please read the following carefully to understand our views and practices regarding your personal data and how we will treat it. By visiting our website and using our services you consent to the practices described in this policy.
+</p>
+<p className="content-title">
+Personal data we process
+</p>
+<p className="content-text with-margin">
+In order to acquire “How to do FinTech in Poland” report and receive the marketing information, you will be asked for basic contact details such as email address and your organisation name.
+</p>
+<p className="content-title">
+Cookies
+</p>
+<p className="content-text with-margin">
+Our website may use cookies to distinguish you from other users of our website. This helps us to provide you with a good experience when you browse our website and also allows us to improve our site. For detailed information on the cookies we use and the purposes for which we use them see our Cookie Policy below.
+</p>
+
+<p className="content-title">
+How we use your personal data
+</p>
+<p className="content-text with-margin">
+We use your personal data to provide you with “How to do FinTech in Poland” report and marketing information regarding our and our business partners’ activity.
+</p>
+
+<p className="content-title">
+Basis of your personal data processing
+</p>
+<p className="content-text with-margin">
+We process your personal data on basis of your consent. You can withdraw the consent at any time.
+</p>
+
+<p className="content-title">
+Disclosure of your personal data
+</p>
+<p className="content-text with-margin">
+We may share your personal data with our subcontractors, including analytics and search engine providers that assist us in the improvement and optimisation of our website.
+</p>
+
+<p className="content-title">
+Place of your personal data processing
+</p>
+<p className="content-text with-margin">
+Your personal data are processed shall be processed destination in Poland or in other county within the European Economic Area ("EEA").
+</p>
+
+<p className="content-title">
+Your rights
+</p>
+<p className="content-text with-margin">
+You have the following specific rights in respect of the personal data we process: <br />
+1. The right to be informed about how we use your personal data - This Privacy Policy explains who we are; the purposes for which we process personal data and our legitimate interests in so doing; the categories of data we process; third party disclosures; <br />
+2. The right of access to your personal data we hold; <br />
+3. The right to rectification where data are inaccurate or incomplete; <br />
+4. The right to erasure of your personal data, but only in very specific circumstances, typically where your personal data are no longer necessary in relation to the purpose for which it was originally collected or processed; or, in certain cases where we have relied on consent to process the data, when that consent is withdrawn and there is no other legitimate reason for continuing to process that data; or when the individual objects to the processing and there is no overriding legitimate interest for continuing the processing; <br />
+5. The right to restrict processing, for example while we are reviewing the accuracy or completeness of data, or deciding on whether any request for erasure is valid. In such cases we shall continue to store the data, but not further process it until such time as we have resolved the issue; <br />
+6. The right to object in cases where processing is based on legitimate interests, where our requirement to process the data is overridden by the rights of the individual concerned; or for the purposes of direct marketing (including profiling); or for processing for purposes of scientific / historical research and statistics, unless this is for necessary for the performance of a public interest task. <br />
+</p>
+<p className="content-text with-margin">
+You can also exercise the right at any time by contacting us at <a href="mailto:info@fintechpoland.com">info@fintechpoland.com</a>.
+</p>
+<p className="content-text with-margin">
+Our website or documents presented at our website may, from time to time, contain links to and from the websites of our partner networks, advertisers and affiliates. If you follow a link to any of these websites, please note that these websites have their own privacy policies and that we do not accept any responsibility or liability for these policies. Please check these policies before you submit any personal data to these websites.
+</p>
+<p className="content-text with-margin">
+If you have any demands, inquiries or complaints about data protection at FinTech Poland Foundation please contact us at <a href="mailto:info@fintechpoland.com">info@fintechpoland.com</a>.
+</p>
+
+<p className="content-title">
+About cookies
+</p>
+<p className="content-text with-margin">
+Cookies are pieces of information that a website transfers to your computer’s hard disk for record-keeping purposes. Cookies can make the internet more useful by storing information about your preferences on a particular site, such as your personal preference pages.
+</p>
+<p className="content-text with-margin">
+The use of cookies is an industry standard, and most websites use them to provide useful features for their customers. Cookies in and of themselves do not personally identify users, although they do identify a user’s computer. Most browsers are initially set to accept cookies.
+</p>
+<p className="content-text with-margin">
+If you would prefer, you can set yours to refuse cookies. However, you may not be able to take full advantage of a website if you do so.
+</p> */}
